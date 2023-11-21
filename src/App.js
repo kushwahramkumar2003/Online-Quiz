c152972bba12d73e23c3080e8b71568e06111365
pankaj143p <pankaj114477pankaj@gmail.com>
2023-11-21 10:51:29 +0530

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Login from "./Components/login"
 import Signup from "./Components/SignUp"
+import Navbar from "./Components/Navbar"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import './App.css';
@@ -9,11 +10,13 @@ function App() {
     
    <Router>
     <Routes>
+    <Route path="/" element={<Navbar/>}></Route>
       <Route exact path="/" element={<Login/>} ></Route>
       <Route exact path="/SignUp" element={<Signup/>}></Route>
 
     </Routes>
    </Router>
+  
 
   );
 }
